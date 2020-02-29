b96e8fb4b08db09dde5c5cb9a8050a5ae887ec60
ngr1 <nagaru335i@gmail.com>
2020-02-29 22:48:28 +0900

    test

---
+++ Calc_20190801/Calc.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calc_20190801
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperation = new System.Windows.Forms.Label();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPlusMinus = new System.Windows.Forms.Button();
+            this.btnRoot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperation
+            // 
+            this.lblOperation.AutoSize = true;
+            this.lblOperation.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblOperation.Location = new System.Drawing.Point(270, 76);
+            this.lblOperation.Name = "lblOperation";
+            this.lblOperation.Size = new System.Drawing.Size(0, 16);
+            this.lblOperation.TabIndex = 0;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtDisplay.Location = new System.Drawing.Point(12, 95);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(306, 23);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn9.Location = new System.Drawing.Point(158, 201);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(61, 43);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn8.Location = new System.Drawing.Point(84, 201);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(68, 43);
+            this.btn8.TabIndex = 3;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn7.Location = new System.Drawing.Point(12, 201);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(66, 43);
+            this.btn7.TabIndex = 4;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn1.Location = new System.Drawing.Point(12, 298);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(66, 45);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn2.Location = new System.Drawing.Point(84, 298);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(68, 45);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn3.Location = new System.Drawing.Point(158, 298);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(61, 45);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn4.Location = new System.Drawing.Point(12, 250);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(66, 42);
+            this.btn4.TabIndex = 10;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn5.Location = new System.Drawing.Point(84, 250);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(68, 42);
+            this.btn5.TabIndex = 9;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn6.Location = new System.Drawing.Point(158, 250);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(61, 42);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnMinus.Location = new System.Drawing.Point(222, 250);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(45, 42);
+            this.btnMinus.TabIndex = 13;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnProduct.Location = new System.Drawing.Point(222, 298);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(45, 45);
+            this.btnProduct.TabIndex = 12;
+            this.btnProduct.Text = "*";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnSum.Location = new System.Drawing.Point(222, 349);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(45, 65);
+            this.btnSum.TabIndex = 11;
+            this.btnSum.Text = "+";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn0.Location = new System.Drawing.Point(12, 349);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(140, 65);
+            this.btn0.TabIndex = 16;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnDot.Location = new System.Drawing.Point(158, 349);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(61, 65);
+            this.btnDot.TabIndex = 15;
+            this.btnDot.Tag = "";
+            this.btnDot.Text = "・";
+            this.btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnDivide.Location = new System.Drawing.Point(222, 201);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(45, 43);
+            this.btnDivide.TabIndex = 14;
+            this.btnDivide.Text = "÷";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnEqual.Location = new System.Drawing.Point(273, 349);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(57, 65);
+            this.btnEqual.TabIndex = 17;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClear.Location = new System.Drawing.Point(273, 300);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 43);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPlusMinus
+            // 
+            this.btnPlusMinus.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPlusMinus.Location = new System.Drawing.Point(273, 202);
+            this.btnPlusMinus.Name = "btnPlusMinus";
+            this.btnPlusMinus.Size = new System.Drawing.Size(57, 43);
+            this.btnPlusMinus.TabIndex = 19;
+            this.btnPlusMinus.Text = "±";
+            this.btnPlusMinus.UseVisualStyleBackColor = true;
+            this.btnPlusMinus.Click += new System.EventHandler(this.BtnPlusMinus_Click);
+            // 
+            // btnRoot
+            // 
+            this.btnRoot.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnRoot.Location = new System.Drawing.Point(273, 251);
+            this.btnRoot.Name = "btnRoot";
+            this.btnRoot.Size = new System.Drawing.Size(57, 43);
+            this.btnRoot.TabIndex = 20;
+            this.btnRoot.Text = "√";
+            this.btnRoot.UseVisualStyleBackColor = true;
+            this.btnRoot.Click += new System.EventHandler(this.BtnRoot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 424);
+            this.Controls.Add(this.btnRoot);
+            this.Controls.Add(this.btnPlusMinus);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnProduct);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.lblOperation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperation;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPlusMinus;
+        private System.Windows.Forms.Button btnRoot;
+    }
+}
+
---
+++ Calc_20190801/Calc.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_20190801
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 演算子を押したか押してないか判定する変数
+        bool opeFlg = false;
+
+        string firstNum = "";
+        string erorr = "エラー";
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (opeFlg) // (opeFlg == true)と同義
+            {
+                txtDisplay.Text = btn1.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn1.Text;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn2.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn2.Text;
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn3.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn3.Text;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn4.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn4.Text;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn5.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn5.Text;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn6.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn6.Text;
+            }
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn7.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn7.Text;
+            }
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn8.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn8.Text;
+            }
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn9.Text;
+                opeFlg = false;
+            }
+            else
+            {
+                txtDisplay.Text += btn9.Text;
+            }
+        }
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            opeFlg = true;
+            firstNum = txtDisplay.Text;
+            lblOperation.Text = btnDivide.Text;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            opeFlg = true;
+            firstNum = txtDisplay.Text;
+            lblOperation.Text = btnMinus.Text;
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            opeFlg = true;
+            firstNum = txtDisplay.Text;
+            lblOperation.Text = btnProduct.Text;
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            opeFlg = true;
+            firstNum = txtDisplay.Text;
+            lblOperation.Text = btnSum.Text;
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+            lblOperation.Text = "";
+            opeFlg = false;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (opeFlg)
+            {
+                txtDisplay.Text = btn0.Text;
+                opeFlg = false;
+            }
+            //else if(txtDisplay.Text=btn0.Text/0)              
+            {
+                txtDisplay.Text += btn0.Text;
+            }
+            if(lblOperation.Text == btnDivide.Text)
+            {
+                txtDisplay.Text = erorr;
+            }
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (lblOperation.Text == btnSum.Text)
+            {
+                txtDisplay.Text = (int.Parse(firstNum) + int.Parse(txtDisplay.Text)).ToString();
+            }
+            else if(lblOperation.Text == btnMinus.Text)
+            {
+                txtDisplay.Text = (int.Parse(firstNum) - int.Parse(txtDisplay.Text)).ToString();
+            }
+            else if (lblOperation.Text == btnDivide.Text)
+            {
+                txtDisplay.Text = (int.Parse(firstNum) / int.Parse(txtDisplay.Text)).ToString();
+            }
+            else if(lblOperation.Text == btnProduct.Text)
+            {
+                txtDisplay.Text = (int.Parse(firstNum) * int.Parse(txtDisplay.Text)).ToString();
+            }
+          
+        }
+
+        private void BtnRoot_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = System.Math.Sqrt(int.Parse(txtDisplay.Text)).ToString();
+        }
+
+        private void BtnPlusMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//0÷=えらーになるようにする
